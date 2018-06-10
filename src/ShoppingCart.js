ce5c5bee11fbfc8ab12ce885e71a8e4c3abceaaa
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-06-10 16:03:14 +0200

    code clean up final touches

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,16 +1,43 @@
-module.exports = function ShoppingCart(...args) {
-  this.items = []
-  this.getItems = () =>  this.items
-  this.addItem =  (itemName,quantity,price) => {
-      const addNewItems = Object.assign({name: itemName,quantity: quantity, pricePerUnit: price})
-      this.items.push(addNewItems)
-      return 
+class ShoppingCart {
+  constructor() {
+    this.items = []
   }
-  this.clear = () =>  this.items = []
-  
+
+  getItems() {
+    return this.items
+  }
+
+  addItem(itemName,quantity,price) {
+    const addNewItems = Object.assign({name: itemName,quantity: quantity, pricePerUnit: price})
+    return this.items.push(addNewItems) 
+  }
+
+  clear() {
+    return this.items = []
+  }
+
+  clone(obj) {
+  let newObj = JSON.parse(JSON.stringify(obj));
+  return newObj
+  }
   
 }
-  
-  
+
+module.exports = ShoppingCart;
+
+// Below data you can use for testing
+
+// const cart = new ShoppingCart()
+// cart.addItem('bike', 1, '200')
+
+// const cart2 = cart.clone(cart)
+// console.log(cart2)
+
+// console.log('.....')
+
+// cart.addItem('bike', 2, '600')
+// cart.addItem('beer', 5, '25')
+// console.log(cart)
+
 
     
--- src/async-functions.js
+++ src/async-functions.js
@@ -1,26 +1,30 @@
-
-module.exports = 'async-functions'
-
 const giveItBackLater = (value, callback) => {
     function loadComplete() {
         callback(value)
     }
-    setTimeout(loadComplete, 10)
+    setTimeout(loadComplete, 200)
 }
-
 
 const addSomePromises = (somePromise) => {
-    return new Promise((resolve,reject) => {
-        if (somePromise=="foo") {
+    let done = true
+    const promise = new Promise((resolve,reject) => {
+        if (done) {
             return resolve(somePromise*2)
-        } else if (somePromise==="bar") {
+        } else {
             return reject(somePromise*3)
         }
-      })
-  }
+    })
+    return promise
 
-const promiseToGiveItBackLater = (value) => {
 }
-
-      
+  
+const promiseToGiveItBackLater = (value) => {
+    return new Promise((resolve,reject) => {
+        function loadComplete() {
+            resolve(value)
+        }
+        setTimeout(loadComplete,700)
+    })
+}
+   
 module.exports = {giveItBackLater,addSomePromises,promiseToGiveItBackLater }
