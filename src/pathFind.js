55831411dd11219c8b92a347ee40569319213e6a
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-06-10 10:13:27 +0200

    Rewrite functionIn previous commit the function was creating nested object from array but the purpose of the exercise was to extract value of the most nested key. I have rewritten the function using reduce to keep the function pure.

--- src/pathFind.js
+++ src/pathFind.js
@@ -1,13 +1,6 @@
-const pathFind = function (object, path) {
-  const value = 'something'
-  if (path.length === 1) {
-    object[path[0]] = v;
-  } else {
-    var key = path.shift();
-    object[key] = pathFind(typeof object[key] === 'undefined' ? {} : object[key], path, value);
-  }
-
-  return object;
+const pathFind = (path,object) => {
+  const reducer = (acc, key) => ((acc && acc[key] !== 'undefined') ? acc[key] : undefined);
+  return path.reduce(reducer, object);
 };
 
 module.exports = {pathFind}
